30d18b1a1a60eb68cd0decb0c661f47dee01cc85
Maks Kaplunovskiy <kaplunovskymv@gmail.com>
2025-08-15 18:00:00 +0300

    implement py-linter-formatter update_1

--- app/main.py
+++ app/main.py
@@ -1,19 +1,15 @@
 def format_linter_error(error: dict) -> dict:
-    return {**{
-        ("line" if error_index == "line_number"
-         else "column" if error_index == "column_number"
-         else "message" if error_index == "text"
-         else "name"): error_value
-        for error_index, error_value in error.items()
-        if error_index in ("line_number", "column_number", "text", "code")},
-        **{"source": "flake8"}}
-
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8",
+    }
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return {**{"errors": [format_linter_error(error) for error in errors]
-               for _ in range(1)},
-            **{"path": file_path, "status": "failed" if errors else "passed"}}
-
+    return {"errors": [format_linter_error(error) for error in errors],
+            "path": file_path, "status": "failed"}
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(report_file, report_error)
